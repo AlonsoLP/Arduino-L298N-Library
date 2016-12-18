4b17808fcf881bb853473b9331b4a68381084565
Alonso Lara <alonso.lara.plana@gmail.com>
2016-12-18 01:52:12 +0100

    L298N version 1.1
    Example updated with new methods.

--- examples/L298N_Test/L298N_Test.ino
+++ examples/L298N_Test/L298N_Test.ino
@@ -10,24 +10,36 @@ const uint8_t IN4 = 11;
 const uint8_t MINSPEED = 50;
 const uint8_t MAXSPEED = 255;
 
-L298N driver(ENA,IN1,IN2,IN3,IN4,ENB, MINSPEED); 
+L298N driver(ENA, IN1, IN2, IN3, IN4, ENB, false, MINSPEED); 
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("FORWARD AT FULL SPEED (255)");
-  driver.move(driver.FORWARD, MAXSPEED, 1000);
-  driver.stop(true, 500);
-  Serial.println("BACKWARD AT FULL SPEED (255)");
-  driver.move(driver.BACKWARD, MAXSPEED, 1000);
-  driver.stop(true, 500);
-  Serial.println("LEFT (STATIONARY)");
-  driver.turn(driver.LEFT, MAXSPEED, -100, 1000);
-  driver.stop(false, 500);
-  Serial.println("RIGHT (STATIONARY)");
-  driver.turn(driver.RIGHT, MAXSPEED, -100, 1000);
-  driver.stop(false, 5000);
+  Serial.println("FORWARD RIGHT");
+  driver.drive(driver.FORWARD_R, MAXSPEED, 40, 1000);
+  driver.drive(driver.STOP, 0, 0, 1000); // stop using drive method
+  Serial.println("BACKWARD RIGHT");
+  driver.drive(driver.BACKWARD_R, MAXSPEED, 40, 1000);
+  driver.stop(false, 1000); // stop using stop method
+  Serial.println("FORWARD LEFT");
+  driver.drive(driver.FORWARD_L, MAXSPEED, 40, 1000);
+  driver.drive(driver.STOP, 0, 0, 1000);
+  Serial.println("BACKWARD LEFT");
+  driver.drive(driver.BACKWARD_L, MAXSPEED, 40, 1000);
+  driver.drive(driver.STOP, 0, 0, 1000);
+  Serial.println("FORWARD");
+  driver.drive(driver.FORWARD, MAXSPEED, 40, 1000);
+  driver.drive(driver.BRAKE, 0, 0, 1000); // brake using drive method
+  Serial.println("BACKWARD");
+  driver.drive(driver.BACKWARD, MAXSPEED, 40, 1000);
+  driver.stop(true, 1000); // brake using stop method
+  Serial.println("RIGHT");
+  driver.drive(driver.RIGHT, MAXSPEED, 40, 1000);
+  driver.drive(driver.STOP, 0, 0, 1000);
+  Serial.println("LEFT");
+  driver.drive(driver.LEFT, MAXSPEED, 40, 1000);
+  driver.drive(driver.STOP, 0, 0, 1000);
 }
 

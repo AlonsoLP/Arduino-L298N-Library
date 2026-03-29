7a8323e6493df4dfbea3c8a5aec605c8663a076d
Alonso Lara <alonso.lara.plana@gmail.com>
2026-03-29 23:28:49 +0200

    v1.22

--- src/L298N.h
+++ src/L298N.h
@@ -1,5 +1,5 @@
 // ---------------------------------------------------------------------------
-// Arduino L298N Library (v1.21)
+// Arduino L298N Library (v1.22)
 //
 // AUTHOR/LICENSE:
 // Created by Alonso José Lara Plana - alonso.lara.plana@gmail.com
@@ -23,6 +23,8 @@
 // * Stops vehicle with/without brakes.
 // * Trajectory correction and turn ratio, adjusting motors speed independently.
 //   It can be used backward and forward (v1.1).
+// * Fully encapsulated pin variables, allowing multiple L298N 
+//   instances (e.g., 4-wheel drive robots) without pin conflicts.
 //
 // CONSTRUCTOR:
 //   L298N driver(ena, in1, in2, in3, in4, enb [, invert][, minspeed]);
@@ -36,7 +38,7 @@
 // METHODS:
 //   COMPLEX:
 //     driver.drive([direction][, speed][, slave_percent][, delay_time])
-//       Send an order to motors. If no direction given, go strait ahead.
+//       Send an order to motors. If no direction given, go straight ahead.
 //       slave_percent (0-100) works only on FORWARD_L, FORWARD_R, BACKWARD_L
 //       and BACKWARD_R. It defines the slave motor speed.
 //         E.g.: speed=150 slave_percent=50 => slave_speed=75
@@ -62,27 +64,25 @@
 //   BRAKE       255  11111111
 //                    ABTT1234  ENA ENB TYPEA TYPEB IN1 IN2 IN3 IN4
 // (*)   TYPE = 1 master - 0 slave
-// (**)  Masters works at speed provided, slave at percent (lower) speed.
+// (**)  Masters work at speed provided, slave at percent (lower) speed.
 // (***) inv = inverted, ENA/ENB = PWM speed if 1
 //
 // TROUBLESHOOTING:
-// * Left is right, right is left! - Exchange motors wires A<->B (hardware method)
-//   or put invert _true_ when create object (software method).
+// * Left is right, right is left! - Exchange motor wires A<->B (hardware method)
+//   or put invert _true_ when creating the object (software method).
 //   E.g.: L298N driver(ENA, IN1, IN2, IN3, IN4, ENB, true, MINSPEED);
-// * Motors does not work! - Remember to use PWM pins for ENA/ENB: 3, 5-6, 9-11
+// * Motors do not work! - Remember to use PWM pins for ENA/ENB: 3, 5-6, 9-11
 //   on most Arduino boards; 2-13 and 44-46 on Arduino Mega.
 //
 // HISTORY:
+// 03/29/2026 v1.22 - Encapsulated global variables into class members and fixed default parameters.
 // 03/11/2018 v1.21 - Cosmetic fixes.
-//
 // 12/19/2016 v1.2 - Added simple methods for basic orders (easy reading code).
 //   Fixed *no need check* in constructor. Cosmetic fixes.
-//
-// 12/18/2016 v1.1 - Code rewrited. Simplified method list. Direction variables
-//   reflects bits used to configure motors, no need to code it separately. You
+// 12/18/2016 v1.1 - Code rewritten. Simplified method list. Direction variables
+//   reflect bits used to configure motors, no need to code it separately. You
 //   can turn backward. Added invert to constructor to avoid motor wire exchange
 //   when left is right and vice versa.
-//
 // 12/16/2016 v1.0 - Initial release.
 // ---------------------------------------------------------------------------
 
@@ -94,26 +94,42 @@
 class L298N
 {
   public:
-    const uint8_t FORWARD = 250, \
-                  FORWARD_R = 234, \
-                  FORWARD_L = 218, \
-                  BACKWARD = 245, \
-                  BACKWARD_R = 229, \
-                  BACKWARD_L = 213, \
-                  RIGHT = 249, \
-                  LEFT = 246, \
-                  STOP = 0, \
+    // Direction and state bitmasks
+    const uint8_t FORWARD = 250, 
+                  FORWARD_R = 234, 
+                  FORWARD_L = 218, 
+                  BACKWARD = 245, 
+                  BACKWARD_R = 229, 
+                  BACKWARD_L = 213, 
+                  RIGHT = 249, 
+                  LEFT = 246, 
+                  STOP = 0, 
                   BRAKE = 255;
-    // constructor
-    L298N (uint8_t ena, uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4, uint8_t enb, boolean invert=false, uint8_t minspeed=0);
-    // complex method (all orders)
-    void drive (uint8_t direction=0, uint8_t speed=255, uint8_t slave_percent=0, int delay_time=0);
-    // simple methods (basic orders)
-    void stop     (boolean brake=false, int delay_time=100);
-    void forward  (uint8_t speed=255, int delay_time=0);
-    void backward (uint8_t speed=255, int delay_time=0);
-    void left     (uint8_t speed=255, int delay_time=200);
-    void right    (uint8_t speed=255, int delay_time=200);
+
+    // Constructor
+    L298N(uint8_t ena, uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4, uint8_t enb, boolean invert = false, uint8_t minspeed = 0);
+    
+    // Complex method handling all movement orders
+    void drive(uint8_t direction = 0, uint8_t speed = 255, uint8_t slave_percent = 0, int delay_time = 0);
+    
+    // Simple methods for basic movement orders
+    void stop(boolean brake = false, int delay_time = 100);
+    void forward(uint8_t speed = 255, int delay_time = 0);
+    void backward(uint8_t speed = 255, int delay_time = 0);
+    void left(uint8_t speed = 255, int delay_time = 200);
+    void right(uint8_t speed = 255, int delay_time = 200);
+
+  private:
+    // Encapsulated pin definitions and configuration to allow multiple class instances
+    uint8_t _ena;
+    uint8_t _enb;
+    uint8_t _in1;
+    uint8_t _in2;
+    uint8_t _in3;
+    uint8_t _in4;
+    
+    boolean _invert;
+    uint8_t _minspeed;
 };
 
 #endif
